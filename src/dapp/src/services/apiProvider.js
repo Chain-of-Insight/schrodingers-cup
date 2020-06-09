4029e1ab800121b72c3dccd709e2fe60db4a8762
Drew Taylor <drew@infiniteinternet.ca>
2020-06-09 18:05:50 -0400

    working version of API compile nomsu as json request body

--- src/dapp/src/services/apiProvider.js
+++ src/dapp/src/services/apiProvider.js
@@ -7,16 +7,16 @@ const API_URL = process.env.API_BASE_URL;
  * @param {String} code : A nomsu payload to send to the API for test execution
  */
 async function testNomic(code) {
-    if (typeof code !== 'string') {
-        return 'Payload rejected. String required, got ' + typeof code;
-    }
+  if (typeof code !== 'string') {
+    return 'Payload rejected. String required, got ' + typeof code;
+  }
 
-    let apiEndpoint = API_URL + 'test';
-    const res = await axios.post(apiEndpoint, {code: code});
-
-    return res;
+  let apiEndpoint = API_URL + 'test';
+  const res = await axios.post(apiEndpoint, {code: code});
+  
+  return res;
 };
 
 module.exports = {
-    testNomic: testNomic
+  testNomic: testNomic
 };
