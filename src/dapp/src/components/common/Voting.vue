7466530ef17f1d7f914ba2a489c2c40b87e2c8e6
Chris Lackey <clackey@runbox.com>
2020-06-16 14:05:21 -0400

    static modal backdrop

--- src/dapp/src/components/common/Voting.vue
+++ src/dapp/src/components/common/Voting.vue
@@ -12,14 +12,11 @@
     </div>
   
     <!-- Modal -->
-    <div class="modal fade" id="voting-modal" tabindex="-1" role="dialog" aria-labelledby="voting-modal-label" aria-hidden="true">
+    <div class="modal fade" id="voting-modal" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog" aria-labelledby="voting-modal-label" aria-hidden="true">
       <div class="modal-dialog modal-lg">
         <div class="modal-content">
           <div class="modal-header">
             <h5 class="modal-title" id="voting-modal-label">Time to vote!</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
           </div>
           <div class="modal-body">
             ...
