5bc3181650f9474c2afb630c97665b19067d9aeb
Chris Lackey <clackey@runbox.com>
2020-06-24 13:25:34 -0400

    some quick fixes to voting and rule proposal prompt

--- src/dapp/src/components/common/Totals.vue
+++ src/dapp/src/components/common/Totals.vue
@@ -4,11 +4,11 @@
     <p class="h2 m-0">Yes: <span class="badge badge-pill badge-success">{{ yes }}</span></p>
     <p class="h2 m-0">No: <span class="badge badge-pill badge-danger">{{ no }}</span></p>
     <p class="h2 m-0">Abstain: <span class="badge badge-pill badge-secondary">{{ abstain }}</span></p>
-    <Countdown
+    <!-- <Countdown
       :duration="turnWindow"
       v-on:ended="endRound"
       ref="timer"
-    ></Countdown>
+    ></Countdown> -->
   </div>
 </template>
 
