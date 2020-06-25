7da70ebe001a4ba51b62282576b2ea157531bb53
Chris Lackey <clackey@runbox.com>
2020-06-25 12:01:00 -0400

    small change to regex pattern for chat msg parsing

--- src/dapp/src/constants/constants.js
+++ src/dapp/src/constants/constants.js
@@ -25,10 +25,11 @@ export const voteTypes = {
 
 export const TZ_WALLET_PATTERN = "(tz(?:1|2|3)[a-zA-Z0-9]{33})";
 
+// XXX: removed regex end-of-string token ('$') on these for now while testing 
 export const eventMessages = {
-  RULE_PROPOSED: `^${TZ_WALLET_PATTERN} proposed a rule in round (\\d+)$`,
-  VOTE_CAST: `^${TZ_WALLET_PATTERN} successfully voted (YES|NO) in round (\\d+)$`,
-  ROUND_OVER: `^Round (\\d+) has concluded$`
+  RULE_PROPOSED: `^${TZ_WALLET_PATTERN} proposed a rule in round (\\d+)`,
+  VOTE_CAST: `^${TZ_WALLET_PATTERN} successfully voted (YES|NO) in round (\\d+)`,
+  ROUND_OVER: `^Round (\\d+) has concluded`
 }
 
 export const CURRENT_RULES = require('../rules/currentRules.json');
